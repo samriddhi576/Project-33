a7665ed525917d20b32faee6986e9df73d937a04
samriddhi576 <87263235+samriddhi576@users.noreply.github.com>
2021-09-24 09:47:01 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Project-33
+# TowerSiege-1
---
+++ Slingshot.js
@@ -0,0 +1,34 @@
+class Slingshot
+{
+    constructor(body1, point2)
+    {
+       var options = {'bodyA': body1, 'pointB': point2, 'stiffness': 0.04, 'length': 1};
+       this.sling = Constraint.create(options);
+       World.add(world,this.sling);
+    }
+
+    attach(body)
+    {
+        this.sling.bodyA = body;
+    }
+    fly()
+    {
+        this.sling.bodyA = null;
+    }
+    display()
+    {
+        if(this.sling.bodyA)
+        {
+            // pointA = this.sling.bodyA.position;
+            // pointB = this.pointB;
+
+             strokeWeight(4);
+             fill("red");
+             //line(pointA.x , pointA.y, pointB.x , pointB.y);
+             line(this.sling.bodyA.position.x, this.sling.bodyA.position.y,this.sling.pointB.x, this.sling.pointB.y);
+          
+        }
+    }
+   
+}
+
